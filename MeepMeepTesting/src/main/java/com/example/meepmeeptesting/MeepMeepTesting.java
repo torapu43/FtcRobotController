b827be9d8b5d6b67bea8a9b8dc67882b0acb6c39
torapu <54970417+torapu43@users.noreply.github.com>
2023-11-26 01:17:30 -0900

    test more auto trajectories

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -10,16 +10,30 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity blueBoard = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 12.46)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(16.875, 64.4375, 90))
+                        drive.trajectorySequenceBuilder(new Pose2d(16.875, 64.4375, Math.toRadians(90)))
+
                                 .setReversed(true)
-                                .splineTo(new Vector2d(5,30),Math.toRadians(210))
+
+//                                .splineTo(new Vector2d(24,30),Math.toRadians(-90))
+//                                .setReversed(false)
+//                                .forward(6)
+//                                .lineToLinearHeading(new Pose2d(48,42,Math.toRadians(180)))
+
+                                //if middle(2)
+                                .splineTo(new Vector2d(12,30), Math.toRadians(-90))
                                 .setReversed(false)
                                 .forward(6)
-                                .lineToLinearHeading(new Pose2d(48,30,Math.toRadians(180)))
+                                .lineToLinearHeading(new Pose2d(48,36,Math.toRadians(180)))
+
+                                //if right side (3)
+//                                .splineTo(new Vector2d(5,30),Math.toRadians(210))
+//                                .setReversed(false)
+//                                .forward(6)
+//                                .lineToLinearHeading(new Pose2d(48,30,Math.toRadians(180)))
                                 .splineToConstantHeading(new Vector2d(-12,60),Math.toRadians(180))
                                 .lineToConstantHeading(new Vector2d(-12,12))
                                 .lineToConstantHeading(new Vector2d(-55,12))
@@ -31,10 +45,21 @@ public class MeepMeepTesting {
                                 .build()
                 );
 
+        RoadRunnerBotEntity blueFar = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 12.46)
+                .followTrajectorySequence(drive ->
+                                drive.trajectorySequenceBuilder(new Pose2d(-31.125, 64.4375, Math.toRadians(90)))
+
+                                        .setReversed(true)
+                                        .splineTo(new Vector2d(-22,30),Math.toRadians(-90))
+                                                .build());
+
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(blueBoard)
+                .addEntity(blueFar)
                 .start();
     }
 }
