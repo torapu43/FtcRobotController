3eae2a4a83715dedaad0e9b9ec35725a49db63d1
torapu <54970417+torapu43@users.noreply.github.com>
2023-11-26 22:03:30 -0900

    test more auto trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/new3208/BlueAutoClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/new3208/BlueAutoClose.java
@@ -7,12 +7,14 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.oldcode.Lululu;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 
+import java.util.List;
 import java.util.Timer;
 
 @Autonomous
@@ -63,15 +65,56 @@ public class BlueAutoClose extends LinearOpMode {
                 .lineToLinearHeading(new Pose2d(48,30,Math.toRadians(180)))
                 .build();
 
+        drive.trajectorySequenceBuilder(new Pose2d(16.875, 64.4375, Math.toRadians(90)))
+
+                .setReversed(true)
+
+//                                .splineTo(new Vector2d(24,30),Math.toRadians(-90))
+//                                .setReversed(false)
+//                                .forward(6)
+//                                .lineToLinearHeading(new Pose2d(48,42,Math.toRadians(180)))
+
+                //if middle(2)
+                .splineTo(new Vector2d(12,30), Math.toRadians(-90))
+                .setReversed(false)
+                .forward(6)
+                .lineToLinearHeading(new Pose2d(48,36,Math.toRadians(180)))
+
+                //if right side (3)
+//                                .splineTo(new Vector2d(5,30),Math.toRadians(210))
+//                                .setReversed(false)
+//                                .forward(6)
+//                                .lineToLinearHeading(new Pose2d(48,30,Math.toRadians(180)))
+                .splineToConstantHeading(new Vector2d(-12,60),Math.toRadians(180))
+                .lineToConstantHeading(new Vector2d(-12,12))
+                .lineToConstantHeading(new Vector2d(-55,12))
+                .forward(6)
+                .back(7)
+                .splineToConstantHeading(new Vector2d(-12,12),Math.toRadians(0))
+                .splineToConstantHeading(new Vector2d(36,36),Math.toRadians(90))
+
+                .build()
+
+        while(opModeInInit()) {
+            objectPos = detectObject();
+        }
+
         telemetry.addData("Ready for W", "");
         telemetry.update();
+
+
 
         waitForStart();
 
 
-        drive.followTrajectory(toRight);
-        drive.followTrajectory(outRight);
-        drive.followTrajectory(scoreRight);
+        if(objectPos == 1){
+
+        }
+        else{
+            drive.followTrajectory(toRight);
+            drive.followTrajectory(outRight);
+            drive.followTrajectory(scoreRight);
+        }
 
     }
 
@@ -106,4 +149,30 @@ public class BlueAutoClose extends LinearOpMode {
                 .build();
 
     }   // end initDoubleVision()
+
+    private int detectObject() {
+        List<Recognition> currentRecognitions = tfod.getRecognitions();
+        telemetry.addData("# Objects Detected", currentRecognitions.size());
+
+        // Step through the list of recognitions and display info for each one.
+        for (Recognition recognition : currentRecognitions) {
+            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
+            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
+
+            telemetry.addData(""," ");
+            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
+            telemetry.addData("- Position", "%.0f / %.0f", x, y);
+            telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
+            telemetry.update();
+
+            if(x < 300){
+                return 1;
+            }
+            if(x >300){
+                return 2;
+            }
+        }   // end for() loop
+        return 3;
+
+    }
 }
