bda5cd684307297864d01f92edfe94c04308aba3
AS1624 <116099541+AS1624@users.noreply.github.com>
2024-02-22 00:41:19 +0000

    add localizer test opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/new3208/LocalizerTest.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.new3208;
+
+import com.acmerobotics.dashboard.message.redux.StopOpMode;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+import java.util.ArrayList;
+import java.util.Queue;
+
+@Autonomous
+public class LocalizerTest extends LinearOpMode {
+
+    //Objects
+    SampleMecanumDrive drive;
+
+    public void runOpMode(){
+        drive = new SampleMecanumDrive(hardwareMap);
+
+        while(opModeInInit()){
+          drive.setPoseEstimate(new Pose2d(0,0,0));
+        }
+
+        waitForStart();
+        if(opModeIsActive()){
+          while(opModeIsActive()){
+            drive.setWeightedDrivePower(new Pose2d(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x));
+            telemetry.addData("location", drive.getPoseEstimate());
+          }
+        }
+
+    }
+}
